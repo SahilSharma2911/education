105230a799cbc9891d692b8860d4cfc1897acba8
SahilSharma2911 <sahilsharmarksh@gmail.com>
2024-08-18 00:12:09 +0530

    add count up

--- app/about/Why.jsx
+++ app/about/Why.jsx
@@ -1,8 +1,16 @@
+"use client";
 import React from "react";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
 
 const Why = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
   return (
-    <div className="w-[100vw] h-auto md:mt-4">
+    <div ref={ref} className="w-[100vw] h-auto md:mt-4">
       <div className="w-10/12 mx-auto ">
         {/* heading section  */}
         <div className="">
@@ -18,7 +26,7 @@ const Why = () => {
         <div className="my-[1rem] md:my-[3rem] flex flex-col lg:flex-row gap-2 md:gap-4 justify-between text-center">
           <div>
             <h1 className=" text-[60px] leading-[90px] text-[#1F94F3] font-poppins">
-              10,000+
+              {inView && <CountUp end={10000} duration={3} suffix="+" />}
             </h1>
             <p className=" font-jost text-[#5C6066] text-[15px] leading-[21px] font-bold">
               STUDENTS Abroad in last 14 years
@@ -27,7 +35,7 @@ const Why = () => {
           <div className=" bg-[#1F94F3] w-[0.1rem]"></div>
           <div>
             <h1 className=" text-[60px] leading-[90px] text-[#1F94F3] font-poppins">
-              100%
+              {inView && <CountUp end={100} duration={3} suffix="%" />}
             </h1>
             <p className=" font-jost font-semibold text-[#5C6066]">
               Admission Success Rate
@@ -36,7 +44,7 @@ const Why = () => {
           <div className=" bg-[#1F94F3] w-[0.1rem]"></div>
           <div>
             <h1 className=" text-[60px] leading-[90px] text-[#1F94F3] font-poppins">
-              100%
+              {inView && <CountUp end={100} duration={3} suffix="%" />}
             </h1>
             <p className=" font-jost font-semibold text-[#5C6066]">
               VISA SUCCESS RATE
@@ -45,7 +53,7 @@ const Why = () => {
           <div className=" bg-[#1F94F3] w-[0.1rem]"></div>
           <div>
             <h1 className=" text-[60px] leading-[90px] text-[#1F94F3] font-poppins">
-              100%
+              {inView && <CountUp end={100} duration={3} suffix="%" />}
             </h1>
             <p className=" font-jost font-semibold text-[#5C6066]">
               SATISFIED PARENTS
--- components/Home/Testimonials.jsx
+++ components/Home/Testimonials.jsx
@@ -2,32 +2,43 @@
 import React from "react";
 import { MdOutlineStarBorderPurple500 } from "react-icons/md";
 import Slider from "../TestimonialSlider/Slider";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
 
 const Testimonials = () => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
+
   return (
     <>
-      <div className="relative lg:bg-banner bg-center bg-contain bg-no-repeat w-[100vw] h-[60vh]">
+      <div
+        ref={ref}
+        className="relative lg:bg-banner bg-center bg-contain bg-no-repeat w-[100vw] h-[60vh]"
+      >
         <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-full lg:w-10/12 bg-[#FAB819] mx-auto flex flex-col lg:flex-row justify-evenly h-full lg:h-auto py-7">
-          {/* first block  */}
+          {/* first block */}
           <div className="flex flex-col items-center py-[1rem]">
             <h1 className="font-poppins text-[2.2rem] text-[#1B52E0]">
-              100<span className="text-[1rem] font-bold">%</span>
+              {inView && <CountUp end={100} duration={3} suffix="%" />}
             </h1>
             <p className="text-[#5C6066] font-bold">VISA GRANT (2023-24)</p>
           </div>
 
-          {/* second block  */}
+          {/* second block */}
           <div className="flex flex-col items-center py-[1rem]">
             <h1 className="font-poppins text-[2.2rem] text-[#1B52E0]">
-              24<span className="text-[1rem] font-bold">x7</span>
+              {inView && <CountUp end={24} duration={3} />}
+              <span className="text-[1rem] font-bold">x7</span>
             </h1>
             <p className="text-[#5C6066] font-bold">SERVICE</p>
           </div>
 
-          {/* Third block  */}
+          {/* Third block */}
           <div className="flex flex-col items-center py-[1rem]">
             <h1 className="flex items-baseline font-poppins text-[2.2rem] text-[#1B52E0]">
-              5
+              {inView && <CountUp end={5} duration={3} />}
               <span className="text-[1rem]">
                 <MdOutlineStarBorderPurple500 />
               </span>
@@ -35,10 +46,10 @@ const Testimonials = () => {
             <p className="text-[#5C6066] font-bold">GOOGLE RATING</p>
           </div>
 
-          {/* fourth block  */}
+          {/* fourth block */}
           <div className="flex flex-col items-center py-[1rem]">
             <h1 className="font-poppins text-[2.2rem] text-[#1B52E0]">
-              10,000<span className="text-[1rem] font-bold">+</span>
+              {inView && <CountUp end={10000} duration={3} suffix="+" />}
             </h1>
             <p className="text-[#5C6066] font-bold">Students Abroad</p>
           </div>
